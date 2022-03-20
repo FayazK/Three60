e5e4652f8f178b1035e9a70bdcf85cf2e81bc59b
Fayaz K <code@fayazk.com>
2022-03-21 00:36:33 +0500

    change to composition API

--- public/js/app.js
+++ public/js/app.js
@@ -26184,10 +26184,10 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5.use[0]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./resources/js/Pages/generate/simple-password.vue?vue&type=script&lang=js":
-/*!*************************************************************************************************************************************************************************************************************!*\
-  !*** ./node_modules/babel-loader/lib/index.js??clonedRuleSet-5.use[0]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./resources/js/Pages/generate/simple-password.vue?vue&type=script&lang=js ***!
-  \*************************************************************************************************************************************************************************************************************/
+/***/ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5.use[0]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./resources/js/Pages/generate/simple-password.vue?vue&type=script&setup=true&lang=js":
+/*!************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib/index.js??clonedRuleSet-5.use[0]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./resources/js/Pages/generate/simple-password.vue?vue&type=script&setup=true&lang=js ***!
+  \************************************************************************************************************************************************************************************************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
@@ -26195,12 +26195,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony import */ var naive_ui__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! naive-ui */ "./node_modules/naive-ui/es/input/src/Input.js");
-/* harmony import */ var naive_ui__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! naive-ui */ "./node_modules/naive-ui/es/slider/src/Slider.js");
-/* harmony import */ var naive_ui__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! naive-ui */ "./node_modules/naive-ui/es/space/src/Space.js");
-/* harmony import */ var naive_ui__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! naive-ui */ "./node_modules/naive-ui/es/button/src/Button.js");
-/* harmony import */ var naive_ui__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! naive-ui */ "./node_modules/naive-ui/es/message/src/MessageProvider.js");
-/* harmony import */ var naive_ui__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! naive-ui */ "./node_modules/naive-ui/es/message/src/use-message.js");
+/* harmony import */ var naive_ui__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! naive-ui */ "./node_modules/naive-ui/es/input/src/Input.js");
+/* harmony import */ var naive_ui__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! naive-ui */ "./node_modules/naive-ui/es/slider/src/Slider.js");
+/* harmony import */ var naive_ui__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! naive-ui */ "./node_modules/naive-ui/es/space/src/Space.js");
+/* harmony import */ var naive_ui__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! naive-ui */ "./node_modules/naive-ui/es/button/src/Button.js");
+/* harmony import */ var naive_ui__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! naive-ui */ "./node_modules/naive-ui/es/message/src/MessageProvider.js");
+/* harmony import */ var naive_ui__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! naive-ui */ "./node_modules/naive-ui/es/message/src/use-message.js");
 /* harmony import */ var omgopass__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! omgopass */ "./node_modules/omgopass/index.js");
 /* harmony import */ var omgopass__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(omgopass__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _Layouts_AppLayout__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @/Layouts/AppLayout */ "./resources/js/Layouts/AppLayout.vue");
@@ -26213,42 +26213,53 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-var message = (0,naive_ui__WEBPACK_IMPORTED_MODULE_4__.useMessage)();
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ((0,vue__WEBPACK_IMPORTED_MODULE_2__.defineComponent)({
-  components: {
-    NInput: naive_ui__WEBPACK_IMPORTED_MODULE_5__["default"],
-    NSlider: naive_ui__WEBPACK_IMPORTED_MODULE_6__["default"],
-    NSpace: naive_ui__WEBPACK_IMPORTED_MODULE_7__["default"],
-    NButton: naive_ui__WEBPACK_IMPORTED_MODULE_8__["default"],
-    AppLayout: _Layouts_AppLayout__WEBPACK_IMPORTED_MODULE_1__["default"],
-    NMessageProvider: naive_ui__WEBPACK_IMPORTED_MODULE_9__["default"]
-  },
-  data: function data() {
-    return {
-      generatedPassword: '',
-      length: '3'
-    };
-  },
-  methods: {
-    generatePassword: function generatePassword() {
-      this.generatedPassword = omgopass__WEBPACK_IMPORTED_MODULE_0___default()({});
-    },
-    formatTooltip: function formatTooltip(value) {
-      return "".concat(value, "%");
-    },
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+  setup: function setup(__props, _ref) {
+    var expose = _ref.expose;
+    expose();
+    var generatedPassword = (0,vue__WEBPACK_IMPORTED_MODULE_2__.ref)('');
+    var length = (0,vue__WEBPACK_IMPORTED_MODULE_2__.ref)(3);
 
+    function createPassword() {
+      generatedPassword.value = omgopass__WEBPACK_IMPORTED_MODULE_0___default()({});
+    }
+
+    function formatTooltip(value) {
+      "".concat(value, "%");
+    }
     /**
      *
      */
-    copyPassword: function copyPassword() {
-      copy_to_clipboard__WEBPACK_IMPORTED_MODULE_3___default()(this.generatedPassword);
-      (0,naive_ui__WEBPACK_IMPORTED_MODULE_4__.useMessage)().info('Password Copied to Clipboard');
+
+
+    function copyPassword() {
+      copy_to_clipboard__WEBPACK_IMPORTED_MODULE_3___default()(generatedPassword.value); //useMessage().info('Password Copied to Clipboard')
     }
-  },
-  mounted: function mounted() {
-    window.$message = (0,naive_ui__WEBPACK_IMPORTED_MODULE_4__.useMessage)();
+
+    var __returned__ = {
+      generatedPassword: generatedPassword,
+      length: length,
+      createPassword: createPassword,
+      formatTooltip: formatTooltip,
+      copyPassword: copyPassword,
+      NInput: naive_ui__WEBPACK_IMPORTED_MODULE_4__["default"],
+      NSlider: naive_ui__WEBPACK_IMPORTED_MODULE_5__["default"],
+      NSpace: naive_ui__WEBPACK_IMPORTED_MODULE_6__["default"],
+      NButton: naive_ui__WEBPACK_IMPORTED_MODULE_7__["default"],
+      NMessageProvider: naive_ui__WEBPACK_IMPORTED_MODULE_8__["default"],
+      useMessage: naive_ui__WEBPACK_IMPORTED_MODULE_9__.useMessage,
+      generatePassword: (omgopass__WEBPACK_IMPORTED_MODULE_0___default()),
+      AppLayout: _Layouts_AppLayout__WEBPACK_IMPORTED_MODULE_1__["default"],
+      ref: vue__WEBPACK_IMPORTED_MODULE_2__.ref,
+      copy: (copy_to_clipboard__WEBPACK_IMPORTED_MODULE_3___default())
+    };
+    Object.defineProperty(__returned__, '__isScriptSetup', {
+      enumerable: false,
+      value: true
+    });
+    return __returned__;
   }
-}));
+});
 
 /***/ }),
 
@@ -30160,20 +30171,12 @@ var _hoisted_3 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNod
 var _hoisted_4 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)("Copy");
 
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  var _component_n_button = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("n-button");
-
-  var _component_n_input = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("n-input");
-
-  var _component_n_space = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("n-space");
-
-  var _component_app_layout = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("app-layout");
-
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createBlock)(_component_app_layout, null, {
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createBlock)($setup["AppLayout"], null, {
     heading: (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
       return [_hoisted_1];
     }),
     "action-buttons": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-      return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_n_button, null, {
+      return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)($setup["NButton"], null, {
         "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
           return [_hoisted_2];
         }),
@@ -30183,28 +30186,28 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
       })];
     }),
     "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-      return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_n_input, {
+      return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)($setup["NInput"], {
         size: "large",
         type: "text",
         readonly: "",
         placeholder: "Your Generated Password",
-        value: _ctx.generatedPassword,
+        value: $setup.generatedPassword,
         "onUpdate:value": _cache[0] || (_cache[0] = function ($event) {
-          return _ctx.generatedPassword = $event;
+          return $setup.generatedPassword = $event;
         })
       }, null, 8
       /* PROPS */
-      , ["value"]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_n_space, {
+      , ["value"]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)($setup["NSpace"], {
         justify: "center",
         "class": "pt-4"
       }, {
         "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-          return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_n_button, {
+          return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)($setup["NButton"], {
             type: "primary",
             size: "large",
             "class": "text-green-500",
             onClick: _cache[1] || (_cache[1] = function ($event) {
-              return _ctx.generatePassword();
+              return $setup.createPassword();
             })
           }, {
             "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
@@ -30213,10 +30216,10 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
             _: 1
             /* STABLE */
 
-          }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_n_button, {
+          }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)($setup["NButton"], {
             size: "large",
             onClick: _cache[2] || (_cache[2] = function ($event) {
-              return _ctx.copyPassword();
+              return $setup.copyPassword();
             })
           }, {
             "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
@@ -75702,14 +75705,14 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var _simple_password_vue_vue_type_template_id_2ead4e45__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./simple-password.vue?vue&type=template&id=2ead4e45 */ "./resources/js/Pages/generate/simple-password.vue?vue&type=template&id=2ead4e45");
-/* harmony import */ var _simple_password_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./simple-password.vue?vue&type=script&lang=js */ "./resources/js/Pages/generate/simple-password.vue?vue&type=script&lang=js");
+/* harmony import */ var _simple_password_vue_vue_type_script_setup_true_lang_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./simple-password.vue?vue&type=script&setup=true&lang=js */ "./resources/js/Pages/generate/simple-password.vue?vue&type=script&setup=true&lang=js");
 /* harmony import */ var _Users_fayazk_www_three60_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
 
 
 
 
 ;
-const __exports__ = /*#__PURE__*/(0,_Users_fayazk_www_three60_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(_simple_password_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_simple_password_vue_vue_type_template_id_2ead4e45__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/Pages/generate/simple-password.vue"]])
+const __exports__ = /*#__PURE__*/(0,_Users_fayazk_www_three60_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(_simple_password_vue_vue_type_script_setup_true_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_simple_password_vue_vue_type_template_id_2ead4e45__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/Pages/generate/simple-password.vue"]])
 /* hot reload */
 if (false) {}
 
@@ -76326,18 +76329,18 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./resources/js/Pages/generate/simple-password.vue?vue&type=script&lang=js":
-/*!*********************************************************************************!*\
-  !*** ./resources/js/Pages/generate/simple-password.vue?vue&type=script&lang=js ***!
-  \*********************************************************************************/
+/***/ "./resources/js/Pages/generate/simple-password.vue?vue&type=script&setup=true&lang=js":
+/*!********************************************************************************************!*\
+  !*** ./resources/js/Pages/generate/simple-password.vue?vue&type=script&setup=true&lang=js ***!
+  \********************************************************************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (/* reexport safe */ _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_use_0_node_modules_vue_loader_dist_index_js_ruleSet_0_use_0_simple_password_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_0__["default"])
+/* harmony export */   "default": () => (/* reexport safe */ _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_use_0_node_modules_vue_loader_dist_index_js_ruleSet_0_use_0_simple_password_vue_vue_type_script_setup_true_lang_js__WEBPACK_IMPORTED_MODULE_0__["default"])
 /* harmony export */ });
-/* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_use_0_node_modules_vue_loader_dist_index_js_ruleSet_0_use_0_simple_password_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/babel-loader/lib/index.js??clonedRuleSet-5.use[0]!../../../../node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./simple-password.vue?vue&type=script&lang=js */ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5.use[0]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./resources/js/Pages/generate/simple-password.vue?vue&type=script&lang=js");
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_use_0_node_modules_vue_loader_dist_index_js_ruleSet_0_use_0_simple_password_vue_vue_type_script_setup_true_lang_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/babel-loader/lib/index.js??clonedRuleSet-5.use[0]!../../../../node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./simple-password.vue?vue&type=script&setup=true&lang=js */ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5.use[0]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./resources/js/Pages/generate/simple-password.vue?vue&type=script&setup=true&lang=js");
  
 
 /***/ }),
--- resources/js/Pages/generate/simple-password.vue
+++ resources/js/Pages/generate/simple-password.vue
@@ -1,59 +1,44 @@
 <template>
-    <app-layout>
-      <template v-slot:heading>
-        Simple Password Generator
-      </template>
-      <template v-slot:action-buttons>
-        <n-button>Refresh</n-button>
-      </template>
-      <n-input size="large" type="text" readonly placeholder="Your Generated Password"
-               v-model:value="generatedPassword"></n-input>
-      <n-space justify="center" class="pt-4">
-        <n-button type="primary" size="large" class="text-green-500" @click="generatePassword()">Generate</n-button>
-        <n-button size="large" @click="copyPassword()">Copy</n-button>
-      </n-space>
-    </app-layout>
+  <app-layout>
+    <template v-slot:heading>
+      Simple Password Generator
+    </template>
+    <template v-slot:action-buttons>
+      <n-button>Refresh</n-button>
+    </template>
+    <n-input size="large" type="text" readonly placeholder="Your Generated Password"
+             v-model:value="generatedPassword"></n-input>
+    <n-space justify="center" class="pt-4">
+      <n-button type="primary" size="large" class="text-green-500" @click="createPassword()">Generate</n-button>
+      <n-button size="large" @click="copyPassword()">Copy</n-button>
+    </n-space>
+  </app-layout>
 </template>
 
-<script>
+<script setup>
 import { NInput, NSlider, NSpace, NButton, NMessageProvider } from 'naive-ui'
 import { useMessage } from 'naive-ui'
 import generatePassword from 'omgopass'
 import AppLayout from '@/Layouts/AppLayout'
-import { defineComponent } from 'vue'
+import { ref } from 'vue'
 import copy from 'copy-to-clipboard'
 
-const message = useMessage()
-export default defineComponent({
-  components: { NInput, NSlider, NSpace, NButton, AppLayout,NMessageProvider },
-  data () {
-    return {
-      generatedPassword: '',
-      length: '3',
-    }
-  },
+let generatedPassword = ref('')
+const length = ref(3)
 
-  methods: {
-    generatePassword () {
-      this.generatedPassword = generatePassword({})
-    },
-    formatTooltip: (value) => `${value}%`,
+function createPassword () {
+  generatedPassword.value = generatePassword({})
+}
 
-    /**
-     *
-     */
-    copyPassword () {
-      copy(this.generatedPassword)
-      useMessage().info('Password Copied to Clipboard')
-    }
+function formatTooltip (value) { `${value}%`}
 
-  },
-
-  mounted () {
-    window.$message = useMessage()
-  }
-
-})
+/**
+ *
+ */
+function copyPassword () {
+  copy(generatedPassword.value)
+  //useMessage().info('Password Copied to Clipboard')
+}
 </script>
 
 <style scoped>
